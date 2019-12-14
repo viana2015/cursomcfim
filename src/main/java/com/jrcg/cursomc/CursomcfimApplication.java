74792ba50a352531a04a5e7dc227c96056e0c303
Cristiano Viana <cristiano.gvianati@gmail.com>
2019-12-14 16:14:15 -0300

    Projeto Criado

---
+++ src/main/java/com/jrcg/cursomc/CursomcfimApplication.java
@@ -0,0 +1,13 @@
+package com.jrcg.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcfimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcfimApplication.class, args);
+	}
+
+}
